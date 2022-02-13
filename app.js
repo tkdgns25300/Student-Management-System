273f4d664bde7d3735c096f3d08a716cf6f39e5d
unknown <tkdgns25300@gmail.com>
2022-02-13 22:40:49 +0900

    route 설정

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 
-
 const PORT = 3000;
+
+// routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App');
+})
+
+// get all the tasks
+
+// get a single task
+
+// create a new task
+
+// update a task
+
+// delete a task
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}...`);
