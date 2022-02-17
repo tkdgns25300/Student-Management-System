50b2126c9b17452fe457d2a7a97a642a75451ad2
leesanghun <tkdgns25300@gmail.com>
2022-02-18 00:11:47 +0900

    use try/catch for asynchronous

--- models/Task.js
+++ models/Task.js
@@ -8,7 +8,10 @@ const TaskSchema = new mongoose.Schema({
         trim: true,
         maxlength: [20, 'name can not be more than 20 characters'],
     },
-    completed: Boolean,
+    completed: {
+        type: Boolean,
+        default: false,
+    },
 })
 
 module.exports = mongoose.model('Task', TaskSchema);
