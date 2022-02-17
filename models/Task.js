5544f6990aa1b032c1a0b0f0e5f6f5d6ecba4f92
leesanghun <tkdgns25300@gmail.com>
2022-02-17 23:44:14 +0900

    Schema에 Validation 추가

--- models/Task.js
+++ models/Task.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 
 
 const TaskSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String, 
+        required: [true, 'must provide name'],
+        trim: true,
+        maxlength: [20, 'name can not be more than 20 characters'],
+    },
     completed: Boolean,
 })
 
