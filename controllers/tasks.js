2928e9325dacf50a295dfdfd24906c1f294606ed
leesanghun <tkdgns25300@gmail.com>
2022-02-17 22:59:09 +0900

    Schema, model 생성

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,3 +1,5 @@
+const Task = require('../models/Task');
+
 const getAllTasks = (req, res) => {
     res.send('get all tasks');
 }
@@ -6,8 +8,9 @@ const getTask = (req, res) => {
     res.json({ id: req.params.id });
 }
 
-const createTask = (req, res) => {
-    res.json(req.body);
+const createTask = async (req, res) => {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
 }
 
 const updateTask = (req, res) => {
